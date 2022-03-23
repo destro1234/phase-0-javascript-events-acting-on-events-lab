98568b4a945a3da3a379f0fde2c87cf18ae0d41b
Elias <eliasventura23@gmail.com>
2022-03-23 12:30:31 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+const dodger = document.getElementById('dodger')
+const leftNumbers = dodger.style.left.replace("px", "");
+const left = parseInt(leftNumbers, 10);
+
+function moveDodgerLeft() {
+   
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+function moveDodgerRight() {
+    if (left < (400 - 40)) {
+        dodger.style.left = `${left + 1}px`;
+    }    
+}
+
+
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
